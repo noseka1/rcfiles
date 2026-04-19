c75ff0079d52fb9852b8326892c43a1e822d47ff
Ales Nosek <ales.nosek@gmail.com>
2026-04-19 13:12:41 -0700

    Add lspsage nvim plugin

---
+++ .config/nvim/lua/plugins/lspsaga.lua
@@ -0,0 +1,58 @@
+return {
+  {
+    "nvimdev/lspsaga.nvim",
+    commit = "562d9724e3869ffd1801c572dd149cc9f8d0cc36",
+    pin = true,
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+    enabled = true,
+    config = function()
+      require("lspsaga").setup({
+        lightbulb = {
+          enable = false,
+        },
+        callhierarchy = {
+          layout = "normal",
+        },
+        references = {
+          layout = "normal",
+        },
+      })
+    end,
+    keys = {
+      -- Key mappings
+      {
+        "<leader>sh",
+        mode = { "n" },
+        "<cmd>Lspsaga hover_doc<cr>",
+        desc = "Lspsaga hover_doc",
+      },
+      {
+        "<leader>sf",
+        mode = { "n" },
+        "<cmd>Lspsaga finder<cr>",
+        desc = "Lspsaga finder",
+      },
+      {
+        "<leader>st",
+        mode = { "n" },
+        "<cmd>Lspsaga goto_type_definition<cr>",
+        desc = "Lspsaga goto_type_definition",
+      },
+      {
+        "<leader>sd",
+        mode = { "n" },
+        "<cmd>Lspsaga goto_definition<cr>",
+        desc = "Lspsaga goto_definition",
+      },
+      {
+        "<leader>sc",
+        mode = { "n" },
+        "<cmd>Lspsaga incoming_calls<cr>",
+        desc = "Lspsaga incoming_calls",
+      },
+    },
+  },
+}
